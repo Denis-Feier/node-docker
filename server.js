4c2d63753203fd9745b62a3b142e183f5bfd95f8
DenisuDevForFood <denisfeier98@yahoo.com>
2020-10-30 21:29:13 +0200

    cool stuff

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const path = require('path')
+const fs = require('fs')
 
 const routes = require('./src/route')
 
 const PORT = process.env.PORT || 3000;
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a+' })
 
 const app = express()
+const logger = morgan('common')
+const loggerFile = morgan('common', { stream: accessLogStream })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(logger)
+app.use(loggerFile)
 
 app.use(routes)
 
